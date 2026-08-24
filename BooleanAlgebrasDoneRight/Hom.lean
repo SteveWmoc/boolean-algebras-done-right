cefa4883cdc0ba2664b910ff9f9e15a8ce95bfe0
Steven Sabean <steves1962wmoc@gmail.com>
2026-08-24 11:08:25 -0500

    Add homomorphism module

---
+++ BooleanAlgebrasDoneRight/Hom.lean
@@ -0,0 +1,14 @@
+import BooleanAlgebrasDoneRight.Basic
+
+/-!
+# Intrinsic Boolean homomorphisms
+
+Boolean homomorphisms are to be defined as homomorphisms of the finitary
+Boolean signature: preservation of the primitive Boolean operations and
+constants comes first.  Monotonicity and preservation of the derived lattice
+structure are consequences.
+-/
+
+namespace BooleanAlgebrasDoneRight
+
+end BooleanAlgebrasDoneRight
